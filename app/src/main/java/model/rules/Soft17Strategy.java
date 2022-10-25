9efc0b26787f2248fe2d841c715549ec9d3ad4ba
jacoobanderson <j00cwa@gmail.com>
2022-10-25 23:06:51 +0200

    Create soft17 class

---
+++ app/src/main/java/model/rules/Soft17Strategy.java
@@ -0,0 +1,12 @@
+package model.rules;
+
+import model.Player;
+
+public class Soft17Strategy implements HitStrategy {
+    private static final int hitLimit = 17;
+    
+    public boolean doHit(Player dealer) {
+        //
+      }
+    
+}

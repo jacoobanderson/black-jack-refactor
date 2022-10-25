e869ac1f7787c0d5823635c74394e097d1bc18c6
jacoobanderson <j00cwa@gmail.com>
2022-10-25 23:41:24 +0200

    Fix dohit method for soft17

--- app/src/main/java/model/rules/Soft17Strategy.java
+++ app/src/main/java/model/rules/Soft17Strategy.java
@@ -1,12 +1,24 @@
 package model.rules;
 
+import model.Card;
 import model.Player;
 
 public class Soft17Strategy implements HitStrategy {
     private static final int hitLimit = 17;
     
     public boolean doHit(Player dealer) {
-        //
+      int score = dealer.calcScore();
+
+      for (Card c : dealer.getHand()) {
+        if (score == hitLimit && c.getValue() == Card.Value.Ace) {
+          return true;
+        }
       }
-    
+
+      if (score < hitLimit) {
+        return true;
+      }
+
+      return false;
+    }
 }

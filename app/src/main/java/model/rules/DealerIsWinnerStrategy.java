90e588bbf0b6c1879adb40cb23df9d01e3a4438e
jacoobanderson <j00cwa@gmail.com>
2022-10-27 00:32:58 +0200

    Create dealer wins on equal score method

--- app/src/main/java/model/rules/DealerIsWinnerStrategy.java
+++ app/src/main/java/model/rules/DealerIsWinnerStrategy.java
@@ -1,5 +1,14 @@
 package model.rules;
 
 public class DealerIsWinnerStrategy implements WinnerStrategy {
+
+    public boolean isDealerTheWinner(int dealerScore, int playerScore, int maxScore) {
+        if (playerScore > maxScore) {
+            return true;
+          } else if (dealerScore > maxScore) {
+            return false;
+          }
+          return dealerScore >= playerScore;
+    }
     
 }

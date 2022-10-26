867aa549db9a56181e94208e0fdc7e9ba8ff955b
jacoobanderson <j00cwa@gmail.com>
2022-10-27 00:41:42 +0200

    Create player is winner on equal method

--- app/src/main/java/model/rules/DealerIsWinnerStrategy.java
+++ app/src/main/java/model/rules/DealerIsWinnerStrategy.java
@@ -2,13 +2,13 @@ package model.rules;
 
 public class DealerIsWinnerStrategy implements WinnerStrategy {
 
-    public boolean isDealerTheWinner(int dealerScore, int playerScore, int maxScore) {
-        if (playerScore > maxScore) {
-            return true;
-          } else if (dealerScore > maxScore) {
-            return false;
-          }
-          return dealerScore >= playerScore;
-    }
+  public boolean isDealerTheWinner(int dealerScore, int playerScore, int maxScore) {
+    if (playerScore > maxScore) {
+        return true;
+      } else if (dealerScore > maxScore) {
+        return false;
+      }
+      return dealerScore >= playerScore;
+  }
     
 }
--- app/src/main/java/model/rules/PlayerIsWinnerStrategy.java
+++ app/src/main/java/model/rules/PlayerIsWinnerStrategy.java
@@ -1,5 +1,12 @@
 package model.rules;
 
 public class PlayerIsWinnerStrategy implements WinnerStrategy{
-    
+  public boolean isDealerTheWinner(int dealerScore, int playerScore, int maxScore) {
+    if (playerScore > maxScore) {
+        return true;
+      } else if (dealerScore > maxScore) {
+        return false;
+      }
+      return dealerScore > playerScore;
+  }
 }
